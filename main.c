0aab934c55d6f062c8e0bbfe206a385553b4cf2e
Nithin M.Das <nithin7105@gmail.com>
2025-06-05 22:11:49 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "pico/stdlib.h"
 #include "bsp/board.h"
 #include "tusb.h"
-#include "tud_hid_keyboard.h"
+//#include "tud_hid_keyboard.h"
 
 int main(void) {
     board_init();
@@ -14,18 +14,6 @@ int main(void) {
 
     while (1) {
         tud_task();
-
-        if (tud_hid_ready() && !sent) {
-            // Send a string as keyboard input
-            send_key(HID_KEY_R, MOD_GUI);
-            sleep_ms(1000);  // Wait for the key to be processed
-            // //type notepad.exe and press enter
-            send_string_as_keyboard("cmd.exe\n");
-            sent = true;    
-            sleep_ms(1600); // Wait for the command to be processed
-            gpio_put(LED_PIN, 1); // Turn on the LED to indicate success
-            send_string_as_keyboard("whoami\n");
-            send_key(HID_KEY_ENTER, MOD_NONE);
-        }
+        gpio_put(LED_PIN, 1); // Turn on the LED to indicate success
     }
 }
