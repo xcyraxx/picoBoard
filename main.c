bc950d12783220a98aed619a17bc826087f23c45
Nithin M.Das <nithin7105@gmail.com>
2025-06-06 21:13:20 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
 #include "tusb.h"
 #include "class/hid/hid.h"
 
-void tud_hid_set_report_cb(uint8_t report_id, uint8_t report_type,
-                           uint8_t const* buffer, uint16_t bufsize) {
+void tud_hid_set_report_cb(uint8_t instance,uint8_t report_id, uint8_t report_type, uint8_t const* buffer, uint16_t bufsize) {
+    (void)instance;
     (void)report_id;
     (void)report_type;
     (void)buffer;
     (void)bufsize;
 }
 
-uint16_t tud_hid_get_report_cb(uint8_t report_id, hid_report_type_t report_type,
-                               uint8_t* buffer, uint16_t reqlen) {
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen) {
+    (void)instance;
     (void)report_id;
     (void)report_type;
     (void)buffer;
