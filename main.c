eafe65c666eaf897dbcb470dcb9c9f0682ca60d6
Adil Jaffer <64605907+xcyraxx@users.noreply.github.com>
2025-06-06 21:08:26 +0530

    Update main.c

--- main.c
+++ main.c
@@ -16,16 +16,7 @@ int main(void) {
         tud_task();
 
         if (tud_hid_ready() && !sent) {
-            // Send a string as keyboard input
-            send_key(HID_KEY_R, MOD_GUI);
-            sleep_ms(1000);  // Wait for the key to be processed
-            // //type notepad.exe and press enter
-            send_string_as_keyboard("cmd.exe\n");
-            sent = true;    
-            sleep_ms(1600); // Wait for the command to be processed
-            gpio_put(LED_PIN, 1); // Turn on the LED to indicate success
-            send_string_as_keyboard("whoami\n");
-            send_key(HID_KEY_ENTER, MOD_NONE);
+            gpio_put(LED_PIN, 1);
         }
     }
 }
