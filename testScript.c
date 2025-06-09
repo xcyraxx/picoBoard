d5a7e7a527ec635d3cce444fa890f7fa13274899
Nithin M.Das <nithin7105@gmail.com>
2025-06-09 09:33:50 -0700

    Update testScript.c

--- testScript.c
+++ testScript.c
@@ -3,33 +3,41 @@
 void testScript(){
     reportQueue *q = get_keyboard_queue();
     
-    report_t test[13] = {0};
+    report_t test[20] = {0};
 
     // Win + R
     test[0].modifier= MOD_LGUI;
     test[0].keycode[0]=HID_KEY_R; 
     test[0].delay_ms = 100;
 
+    test[1].keycode[0] = HID_KEY_N;
+    test[2].keycode[0] = HID_KEY_O;
+    test[3].keycode[0] = HID_KEY_T;
+    test[4].keycode[0] = HID_KEY_E;
+    test[5].keycode[0] = HID_KEY_P;
+    test[6].keycode[0] = HID_KEY_A;
+    test[7].keycode[0] = HID_KEY_D;
+
     // Enter
-    test[1].keycode[0] = HID_KEY_ENTER;
-    test[1].delay_ms = 500;
+    test[8].keycode[0] = HID_KEY_ENTER;
+    test[8].delay_ms = 500;
 
     // Text Message
-    test[2].keycode[0] = HID_KEY_W;
-    test[3].keycode[0] = HID_KEY_A;
-    test[4].keycode[0] = HID_KEY_S;
-    test[5].keycode[0] = HID_KEY_S;
-    test[6].keycode[0] = HID_KEY_U;
-    test[7].keycode[0] = HID_KEY_P;
+    test[9].keycode[0] = HID_KEY_W;
+    test[10].keycode[0] = HID_KEY_A;
+    test[11].keycode[0] = HID_KEY_S;
+    test[12].keycode[0] = HID_KEY_S;
+    test[13].keycode[0] = HID_KEY_U;
+    test[14].keycode[0] = HID_KEY_P;
 
-    test[8].keycode[0] = HID_KEY_SPACE;
+    test[15].keycode[0] = HID_KEY_SPACE;
 
-    test[9].keycode[0] = HID_KEY_C;
-    test[10].keycode[0] = HID_KEY_U;
-    test[11].keycode[0] = HID_KEY_H;
-    test[12].keycode[0] = HID_KEY_H;
+    test[16].keycode[0] = HID_KEY_C;
+    test[17].keycode[0] = HID_KEY_U;
+    test[18].keycode[0] = HID_KEY_H;
+    test[19].keycode[0] = HID_KEY_H;
 
-    for(int i = 0; i < 13; i++){
+    for(int i = 0; i < 20; i++){
         enqueue_report(&test[i], q);
     }
 
